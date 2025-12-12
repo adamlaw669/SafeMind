62ee7e10ee27c36e780063607c740312c403b4c9
Emmanuel <emmasco0207@gmail.com>
2025-12-12 03:37:08 +0100

    connecting modals

--- frontend/src/schemas/reportFormValidation.ts
+++ frontend/src/schemas/reportFormValidation.ts
@@ -9,8 +9,8 @@ export const reportFormValidationSchema = z.object({
     .trim()
     .min(50, { message: "Minimum of 50 characters." }),
   attachFile: z
-    .any()
-    .refine((files) => files instanceof FileList, "File is required")
+    .any().optional()
+    .refine((files) => files instanceof FileList, "File is required").optional()
     .refine((files) => files?.length > 0, "Please upload a file").optional(),
   // .refine(
   //   (files) => ["image/png", "image/jpeg"].includes(files[0].type),
