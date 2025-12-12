3dbd5cc16eff6f3e0b0f5b24cbe8181d24dc727d
Emmanuel <emmasco0207@gmail.com>
2025-12-12 03:36:10 +0100

    connecting modals

--- frontend/src/pages/UserDashboard/ModalsForContent/ModalForContent.tsx
+++ frontend/src/pages/UserDashboard/ModalsForContent/ModalForContent.tsx
@@ -2,7 +2,7 @@ import ReportModal from "./ReportModal/ReportModal";
 import TrackModal from "./TrackModal/TrackModal";
 import SupportModal from "./SupportModal/SupportModal";
 import ModalComponent from "../../../component/Modals/ModalComponent";
-import type { ReactNode } from "react";
+
 
 const ModalForContent = ({ popupType, togglePopup, closePopup }: { popupType: string, togglePopup: boolean, closePopup:any }) => {
     // let footerBtns:ReactNode
@@ -17,7 +17,7 @@ const ModalForContent = ({ popupType, togglePopup, closePopup }: { popupType: st
     // }
 
     return (
-        <ModalComponent open={togglePopup} onClose={closePopup} footer={[]}>
+        <ModalComponent open={togglePopup} onClose={closePopup} footer={[]} width="765px" maskClosable={popupType === "report" && false}>
             {popupType === "report" && <ReportModal/>}
             {popupType === "track" && <TrackModal/>}
             {popupType === "support" && <SupportModal/>}
