e6a2e0cefb36df7caef95cd3c10ecaf2480792ea
Emmanuel <emmasco0207@gmail.com>
2025-12-12 03:35:49 +0100

    connecting modals

--- frontend/src/component/FormButtons/ActionButton.tsx
+++ frontend/src/component/FormButtons/ActionButton.tsx
@@ -6,11 +6,12 @@ type ActionButtonProps = {
     children:ReactElement;
     className?: string;
     onClick?: ()=>void;
+    disabled?: boolean
 }
 
-const ActionButton = ({children, type, className, onClick} : ActionButtonProps) => {
+const ActionButton = ({children, type, className, onClick, disabled} : ActionButtonProps) => {
   return (
-    <button type={type} className={`${className} w-fit rounded-full px-3 py-2 cursor-pointer font-semibold text-center flex flex-row justify-center items-center hover:transition-all hover:duration-200m disabled:bg-[#A5A5A5] disabled:cursor-not-allowed`} onClick={onClick}>
+    <button type={type} className={`${className} w-fit rounded-full px-3 py-2 cursor-pointer font-semibold text-center flex flex-row justify-center items-center hover:transition-all hover:duration-200m disabled:bg-[#A5A5A5] disabled:cursor-not-allowed`} onClick={onClick} disabled={disabled}>
         {children}
     </button>
   )
